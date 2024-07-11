02a7f021f03c5169939c7e82988a4dc59c5ccb46
RhymeAccel <dsplayer88@live.com>
2024-07-11 14:26:11 -0400

    Finally got tailwind to work

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,14 @@
 import {StatusBar} from 'expo-status-bar'
-import {StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Go To Profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
-})
