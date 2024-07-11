b5574dea3a192014129f3e272adc1552c0079143
RhymeAccel <dsplayer88@live.com>
2024-07-11 15:19:04 -0400

    Created folder structure + nav bar

--- app/index.jsx
+++ app/index.jsx
@@ -5,9 +5,9 @@ import { Link } from 'expo-router'
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className='text-3xl'>Aora!</Text>
+      <Text className='text-3xl font-pblack'>Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{color: 'blue'}}>Go To Profile</Link>
+      <Link href="/home" style={{color: 'blue'}}>Go To Home</Link>
     </View>
   );
 }
